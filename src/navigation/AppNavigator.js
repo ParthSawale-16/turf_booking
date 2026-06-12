de591ae39011b023e266c3451d8b28275b43475c
Parth_Sawale_16 <parthsawale24@gmail.com>
2026-06-12 11:24:24 +0530

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,64 +2,29 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignupScreen';
-import HomeScreen from '../screens/HomeScreen';
-import SearchScreen from '../screens/SearchScreen';
-import BookingScreen from '../screens/BookingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-
       <Stack.Navigator
-        initialRouteName="Login"
         screenOptions={{
-          headerShown: true,   // ✅ BACK BUTTON ENABLED
+          headerShown: false
         }}
       >
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+        />
 
-        {/* LOGIN */}
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ headerShown: false }}
         />
-
-        {/* SIGNUP */}
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-
-        {/* HOME */}
-        <Stack.Screen
-  name="Home"
-  component={HomeScreen}
-  options={{
-    headerShown: false
-  }}
-/>
-
-        {/* SEARCH */}
-        <Stack.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{ title: 'Search' }}
-        />
-
-        {/* BOOKING */}
-        <Stack.Screen
-          name="Booking"
-          component={BookingScreen}
-          options={{ title: 'Booking' }}
-        />
-
       </Stack.Navigator>
-
     </NavigationContainer>
   );
 }
