f3b406dfb7d14d2f3540f45d0f4d2d0910ea68e3
Parth_Sawale_16 <parthsawale24@gmail.com>
2026-06-29 12:37:21 +0530

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,26 +4,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import HomeScreen from '../screens/HomeScreen';
+import TurfDetailsScreen from '../screens/TurfDetailsScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import SearchScreen from '../screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function Navigation() {
+export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-        />
-
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-        />
+  screenOptions={{
+    headerShown: false,
+    animation: 'slide_from_right',
+  }}
+>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup"component={SignupScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TurfDetails" component={TurfDetailsScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="SearchScreen" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
